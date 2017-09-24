7881401991f16e04d238a5e073be464be94c1c85
quyencao <boy20195@gmail.com>
2017-09-24 22:38:29 +0700

    Shop Index

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function(){
+  $('.slider').slick({
+    adaptiveHeight: true,
+    infinite: true,
+    dots: true,
+    slidesToShow: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+});
